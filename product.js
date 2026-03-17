47ed4995b608d32223d0297a1688ac2f0feee0ab
Ilya06 <ilyagrigorov2006@gmail.com>
2026-03-18 02:56:55 +0500

    upgrade

---
+++ product.js
@@ -0,0 +1,62 @@
+export const productsInfo = [
+  {
+    image: "images/moisturizing-mousse.png",
+    forSkin: 'для нормальной кожи',
+    title: 'Увлажняющий мусс',
+    description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+    composition: [
+      'активные натуральные комплексы',
+      'витамины С, А, РР, В И Е',
+      'солнцезащитные компоненты'
+    ],
+    price: 2750
+  },
+  {
+    image: "images/moisturizing-mask.png",
+    forSkin: 'для нормальной кожи',
+    title: 'Увлажняющая маска',
+    description: 'Способствует удерживанию влаги в верхних слоях кожи.',
+    composition: [
+      'воски',
+      'минералы',
+      'масла'
+    ],
+    price: 3500
+  },
+  {
+    image: "images/gel-for-washing.png",
+    forSkin: 'для нормальной кожи',
+    title: 'Гель для умывания',
+    description: 'Интенсивно очищает, не повреждает защитный барьер кожи.',
+    composition: [
+      'минералы',
+      'витамины С, А, РР, В И Е',
+      'солнцезащитные компоненты'
+    ],
+    price: 1650
+  },
+  {
+    image: "images/gift-set-1.png",
+    forSkin: 'для нормальной кожи',
+    title: 'Подарочный набор №1',
+    description: 'Набор, состоящий из увлажняющего крема и маски.',
+    composition: [
+      'воски',
+      'минералы',
+      'масла'
+    ],
+    price: 4750
+  },
+  {
+    image: "images/gift-set-2.png",
+    forSkin: 'для нормальной кожи',
+    title: 'Подарочный набор №5',
+    description: 'Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.',
+    composition: [
+      'воски',
+      'минералы',
+      'масла'
+    ],
+    price: 7520
+  }
+];
